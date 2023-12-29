89e2a80bfa670917ee1841276cc595ca96b98b99
Mahmoud Hashim <mahmoud.ios.dev@gmail.com>
2023-12-29 15:33:45 +0200

    adding the nested routes"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -6,29 +6,37 @@ import BookList from './pages/BookList';
 import Book from './pages/Book';
 import NewBook from "./pages/NewBook";
 import NotFound from "./pages/NotFound";
+import BooksLayout from "./pages/BooksLayout";
 
 
 
 function App() {
   return <>
-      <nav>
-        <ul>
-          <li><Link to={"/"}>Home</Link></li>
-          <li><Link to={"/about"}>About</Link></li>
-          <li><Link to={"/contact"}>Contact</Link></li>
-          <li><Link to={"/bookList"}>BookList</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/bookList" element={<BookList />} />
-        {/* NOTE => id is the variable that will be changed with every new book */}
-        <Route path="/bookList/:id" element={<Book />} />
-        <Route path="/bookList/newBook" element={<NewBook />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+    {/* Fixed */}
+    <nav>
+      <ul className="nav-ul">
+        <li className="nav-bar-li"><Link to={"/"}>Home</Link></li>
+        <li className="nav-bar-li"><Link to={"/contact"}>Contact</Link></li>
+        <li className="nav-bar-li"><Link to={"/bookList"}>BookList</Link></li>
+        <li className="nav-bar-li"><Link to={"/about"}>About</Link></li>
+
+      </ul>
+    </nav>
+    {/* Rendering */}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/bookList" element={<BooksLayout />}>
+      {/* sub Route of the parent route which is the BookList Route */}
+        {/*The original route defined by this method below:  */}
+        <Route path=":id" element={<Book />} />
+        <Route index element={<BookList/>} />
+        <Route path="newBook" element={<NewBook />} />
+      </Route>
+      {/* NOTE => id is the variable that will be changed with every new book */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   </>
 }
 
--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -3,12 +3,5 @@ import { Link } from "react-router-dom";
 export default function BookList() {
     return <>
         <h1>BookList</h1>
-        <ul>
-            <li><Link to="/bookList/1">Book1</Link></li>
-            <li><Link to="/bookList/2">Book2</Link></li>
-            <li><Link to="/bookList/3">Book3</Link></li>
-            <li><Link to="/bookList/newBook">New Book</Link></li>
-        </ul>
-
     </>
 }
